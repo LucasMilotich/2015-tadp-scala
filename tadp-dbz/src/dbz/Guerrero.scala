72b5d22d35d36584b65abf0bb46430e572b53c1f
emiiSierra <sierraemii@gmail.com>
2015-11-01 20:54:24 -0300

    refactor 2

--- tadp-dbz/src/dbz/Guerrero.scala
+++ tadp-dbz/src/dbz/Guerrero.scala
@@ -3,12 +3,16 @@ package dbz
 case class Guerrero(
       nombre: String,
       tipo:Tipo,
+      ki: Int,
       maximoKi: Int,
       items:List[Item] = List()) {
+     
+  def aumentarKi (numero: Int) =
+    copy(ki = Math.min(ki + numero, maximoKi))
+    
+  def cargarKi = 
+    tipo.subirKi(this)
   
-  var ki = maximoKi
-   
-  def aumentarKi (numero: Int) = copy(maximoKi = Math.min(ki + numero, maximoKi))
-      
+    
 }
   
--- tadp-dbz/src/dbz/Movimiento.scala
+++ tadp-dbz/src/dbz/Movimiento.scala
@@ -5,12 +5,14 @@ trait Movimiento extends Function2[Guerrero,Guerrero,Guerrero] {
  }
  
   case object DejarseFajar extends Movimiento {
-   override def apply(guerrero: Guerrero, oponente: Guerrero) = guerrero
-   
+   override def apply(guerrero: Guerrero, oponente: Guerrero) = {
+     println("pegame y decime " + guerrero.nombre)
+     guerrero
+   }
  }
  
   case object CargarKi extends Movimiento {
     override def apply(guerrero: Guerrero, oponente: Guerrero) = {
-      guerrero.tipo.subirKi(guerrero)
+      guerrero.cargarKi
     }
   }
